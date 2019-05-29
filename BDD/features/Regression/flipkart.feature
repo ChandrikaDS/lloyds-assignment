5998aeb19c58dc5542eeef1fe0d0ee7398ab640f
ChandrikaDS <chandrikads7@gmail.com>
2019-05-29 21:21:14 +0530

    Updating feature file

--- BDD/features/Regression/flipkart.feature
+++ BDD/features/Regression/flipkart.feature
@@ -8,7 +8,12 @@ Feature: Flipkart application to add the product to the cart
         Given User lands on the Flipkart application
 
     Scenario: Adding a product to cart
-        When User search for Iphone from Search box in HomePage
-        Then the Results displays
-       
+        When User search for <brand> from Search box in HomePage
+        Then User should be able to see the required search results
+        And User should be able to select "<iphone>" successfully
+        Then User should be able to land in the device description page
+        And User should be able to click on Add to cart button
 
+       Examples:
+       | brand | iphone | 
+       | iphone  |  Apple iPhone 7 Plus (Silver, 32 GB)  | 
--- BDD/features/step-definitions/stepdef.js
+++ BDD/features/step-definitions/stepdef.js
@@ -7,9 +7,19 @@ Given(/^User lands on the Flipkart application$/, function() {
     browser.pause(1000)
     browser.maximizeWindow();
 });
-When(/^User search for Iphone from Search box in HomePage$/,function(){
-    console.log("im here+++++++++++++++++++++");
+When(/^User search for \"([^\"]*)\" from Search box in HomePage$/,function(brand){
+    console.log("search for ");
 });
 Then(/^the Results displays$/, function() {
-    console.log("im her77777777777777777");
+    console.log("");
+});
+
+Then(/^User should be able to select \"([^\"]*)\" successfully$/, function(iphone) {
+    console.log("");
+});
+Then(/^User should be able to land in the device description page$/, function() {
+    console.log("");
+});
+Then(/^User should be able to click on Add to cart button$/, function() {
+    console.log("");
 });
