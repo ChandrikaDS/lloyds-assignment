20ae2208a3586aa39a2ed576010851e2c5b684af
ChandrikaDS <chandrikads7@gmail.com>
2019-05-31 15:41:40 +0530

    updated stepdef file - test for adding iphone to cart

--- BDD/features/step-definitions/stepdef.js
+++ BDD/features/step-definitions/stepdef.js
@@ -1,25 +1,61 @@
 const { Given, When, Then } = require('cucumber')
 
 //require(chai).should();
-console.log("*********************************")
 Given(/^User lands on the Flipkart application$/, function() {
     browser.url("https://www.flipkart.com")
-    browser.pause(1000)
-    browser.maximizeWindow();
+    browser.$('//button[@class="_2AkmmA _29YdH8"]').click();
+
 });
 When(/^User search for \"([^\"]*)\" from Search box in HomePage$/,function(brand){
-    console.log("search for ");
-});
-Then(/^the Results displays$/, function() {
-    console.log("");
+    const searchbox = browser.$('//input[@title="Search for products, brands and more"]')
+    searchbox.click();
+    searchbox.setValue(brand)
+    browser.$('//button[@class="vh79eN"]').click();
+    browser.pause(3000);
+  
 });
 
-Then(/^User should be able to select \"([^\"]*)\" successfully$/, function(iphone) {
-    console.log("");
-});
+Then(/^User should be able to select \"([^\"]*)\" successfully$/,function(iphone){
+    
+    let noOfPages = browser.$("(//*[@class='_2zg3yZ']//span)[1]").getText().split("of ")[1];
+    console.log(noOfPages);
+    let found = false;
+    let i = 1;
+        
+    while(i <= noOfPages){
+        console.log(`${i}`);
+        var phoneElem  = browser.$('//div[text()="' + iphone +'"]');
+     
+        if(! phoneElem.error){
+            found = true;
+            browser.$(`//*[@class='_3wU53n' and text()="${iphone}"]/ancestor::a`).click();
+            break;
+        }else{
+            i++;
+            browser.$(`(//*[@class='_2Xp0TH'])[${i}]`).click();
+            browser.pause(1000);
+        }
+    }
+    
+    if(!found) {
+        console.log(`${iphone} not found!`);
+    }
+}); 
+
 Then(/^User should be able to land in the device description page$/, function() {
-    console.log("");
+browser.pause(5000);
+    console.log(browser.getWindowHandles());
+    browser.switchToWindow(browser.getWindowHandles()[1]);
+    console.log(browser.getUrl());
 });
-Then(/^User should be able to click on Add to cart button$/, function() {
-    console.log("");
+
+Then(/^User should be able to click on Add to cart button in \"([^\"]*)\" pdp page$/, function(iphone) {
+    var addtoCart = browser.$("//*[text()='ADD TO CART']");
+    if(!addtoCart.error){
+        addtoCart.click();
+    }
+    else{
+        console.log("Element is out of stock");
+    }
+    browser.pause(5000);
 });
